1d27387875dcc89b033532d84972ac5cce4a26c3
Carlos Henrique Benício Costa <101262618+Carlos-Henrique39@users.noreply.github.com>
2022-11-28 09:56:19 -0300

    Update Copo.java

--- Engenharia 3/Composite/patern/src/pattern/Copo.java
+++ Engenharia 3/Composite/patern/src/pattern/Copo.java
@@ -3,6 +3,6 @@ package pattern;
 public class Copo implements Compra {
 	@Override
 	public void buy() {
-		System.out.println("Comprando copo...");
+		System.out.println("Comprando um copo de...");
 	}
 }
